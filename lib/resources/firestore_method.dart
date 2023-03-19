96737cb395ba28925f0be4e1a16a870691c7a0c3
Golam Sarwer <golamsarwer97@gmail.com>
2023-03-19 13:21:33 +0600

    create firestore method

--- lib/resources/firestore_method.dart
+++ lib/resources/firestore_method.dart
@@ -1,49 +1,58 @@
-// import 'dart:typed_data';
-//
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:uuid/uuid.dart';
-//
-// class FirestoreMethod {
-//   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-//
-//   Future<String> addProduct(
-//       String
-//     String productId,
-//     String productName,
-//     String productQuantity,
-//     String productPrice,
-//     String productDescription,
-//   ) async {
-//     String res = "Some error occurred";
-//     try {
-//       if (productName.isNotEmpty) {
-//         String productId = const Uuid().v1();
-//         await _firestore
-//             .collection('orders')
-//             .doc(orderId)
-//             .collection('products')
-//             .doc(productId)
-//             .set({
-//           'userId': userId,
-//           'userName': userName,
-//           'userProfileImage': userProfileImage,
-//           'postId': postId,
-//           'cmntId': cmntId,
-//           'cmntText': cmntText,
-//           'cmntDatePublished': DateTime.now(),
-//           'cmntLikes': [],
-//         });
-//
-//         res = "Success";
-//       } else {
-//         res = 'Text is empty';
-//       }
-//     } catch (e) {
-//       res = e.toString();
-//     }
-//     return res;
-//   }
-//
-//
-//
-// }
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:nbt/providers/product.dart';
+import 'package:uuid/uuid.dart';
+
+class FirestoreMethod {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<String> addProduct(
+    String productId,
+    String productName,
+    String productQuantity,
+    String productPrice,
+    String productDescription,
+    String orderId,
+  ) async {
+    String res = "Some error occurred";
+    try {
+      if (productName.isNotEmpty) {
+        String productId = const Uuid().v1();
+        Product product = Product(
+          id: productId,
+          name: productName,
+          quantity: productQuantity,
+          price: productPrice,
+          description: productDescription,
+        );
+        await _firestore
+            .collection('orders')
+            .doc(orderId)
+            .collection('products')
+            .doc(productId)
+            .set(product.toJson());
+
+        res = "Success";
+      } else {
+        res = 'Text is empty';
+      }
+    } catch (e) {
+      res = e.toString();
+    }
+    return res;
+  }
+
+  Future<void> deleteProduct(String orderId, String productId) async {
+    try {
+      await _firestore
+          .collection('orders')
+          .doc(orderId)
+          .collection('products')
+          .doc(productId)
+          .delete();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
